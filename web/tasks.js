588e86b32a5c3f49b870507540fbf25e81376552
MORION123 <100638576+MORION123@users.noreply.github.com>
2026-03-27 13:26:12 +0500

    Create tasks.js

---
+++ web/tasks.js
@@ -0,0 +1,14 @@
+const tasks = [
+    {
+        question: "2 + 2 = ?",
+        answer: "4"
+    },
+    {
+        question: "5 * 3 = ?",
+        answer: "15"
+    },
+    {
+        question: "10 / 2 = ?",
+        answer: "5"
+    }
+];
