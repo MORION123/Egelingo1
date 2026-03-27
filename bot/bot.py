06c57403ebf5bc4d37aede58ab96f7e5df03a281
MORION123 <100638576+MORION123@users.noreply.github.com>
2026-03-27 13:30:20 +0500

    Create bot.py

---
+++ bot/bot.py
@@ -0,0 +1,37 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils import executor
+
+API_TOKEN = "8771751592:AAHVJR4xjomMF3eXwjVbjcpqmClT-qA_KSk
+"
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+# 🔘 Кнопка WebApp
+kb = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(
+        text="Открыть тренажёр",
+        web_app=WebAppInfo(url="https://username.github.io/telegram-ege-app/")
+    )]
+])
+
+
+# ▶️ Команда /start
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer(
+        "📘 Тренажёр ЕГЭ по математике\n\nНажми кнопку ниже 👇",
+        reply_markup=kb
+    )
+
+
+# 📥 Получение данных из WebApp
+@dp.message_handler(content_types=types.ContentType.WEB_APP_DATA)
+async def web_app_data(message: types.Message):
+    await message.answer(f"📊 Результат: {message.web_app_data.data}")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
